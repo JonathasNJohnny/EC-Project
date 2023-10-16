586e5ae6670bd810af2f460a908d81bbcb5c8669
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-16 00:52:46 -0300

    Add files via upload

---
+++ clinica_react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Clinic from "./components/Clinic"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Clinic></Clinic>
+    </div>
+  );
+}
+
+export default App;
