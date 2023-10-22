2f62b104d59e8a51d9d8c312456fa81eb9430d8c
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-22 16:49:22 -0300

    Add files via upload

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
-import usersFunc
+from functions.login import login
 
 app = Flask(__name__)
 
 @app.route("/api/login", methods=['GET'])
-def login():
+def login_route():
     name = request.args.get('name')
     password = request.args.get('password')
-    result = usersFunc.login(name, password)
+    result = login(name, password)
     return jsonify({"message": result})
 
 if __name__ == "__main__":
