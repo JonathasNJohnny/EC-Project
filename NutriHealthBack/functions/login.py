e1e9300c6775c1e954e2ad9199a3122ffff3e28a
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-27 00:29:54 -0300

    Add files via upload

--- NutriHealthBack/functions/login.py
+++ NutriHealthBack/functions/login.py
@@ -1,21 +1,31 @@
 from schemes.schemes import *
+import json
 
 #Obter IDs de Usuários - Start
-receptionistID = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Recepcionista').get().tipo_usuario_id
-doctorID = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Medico').get().tipo_usuario_id
-administratorID = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Administrador').get().tipo_usuario_id
-patientID = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Paciente').get().tipo_usuario_id
+receptionist = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Recepcionista').get()
+receptionist = [receptionist.tipo_usuario_id, receptionist.tipo_usuario_nome]
+doctor = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Medico').get()
+doctor = [doctor.tipo_usuario_id, doctor.tipo_usuario_nome]
+administrator = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Administrador').get()
+administrator = [administrator.tipo_usuario_id, administrator.tipo_usuario_nome]
+patient = TiposUsuarios.select().where(TiposUsuarios.tipo_usuario_nome == 'Paciente').get()
+patient = [patient.tipo_usuario_id, patient.tipo_usuario_nome]
 #Obter IDs de Usuários - End
+
+#Função para retornar um Json do usuário - Start
+def getJson(user_id, user_type):
+    return json.dumps({"user_id": user_id, "user_type": user_type})
+#Função para retornar um Json do usuário - End
 
 #Funções de retorno de cada usuário - Start
 def receptionistUser():
-    return "Recepcionista!"
+    return getJson(receptionist[0], receptionist[1])
 def doctorUser():
-    return "Médico!"
+    return getJson(doctor[0], doctor[1])
 def administratorUser():
-    return "Administrador!"
+    return getJson(administrator[0], administrator[1])
 def patientUser():
-    return "Paciente!"
+    return getJson(patient[0], patient[1])
 #Funções de retorno de cada usuário - End
 
 #Função para retornar o usuário - Start
@@ -31,14 +41,14 @@ def getUser(username, password):
 def login(username, password):
     user = getUser(username, password)
     if user:
-        if user.tipo_usuario_id.tipo_usuario_id == receptionistID:
-            return "Seja bem vindo ao seu perfil de " + receptionistUser()
-        elif user.tipo_usuario_id.tipo_usuario_id == doctorID:
-            return "Seja bem vindo ao seu perfil de " + doctorUser()
-        elif user.tipo_usuario_id.tipo_usuario_id == administratorID:
-            return "Seja bem vindo ao seu perfil de " + administratorUser()
-        elif user.tipo_usuario_id.tipo_usuario_id == patientID:
-            return "Seja bem vindo ao seu perfil de " + patientUser()
+        if user.tipo_usuario_id.tipo_usuario_id == receptionist[0]:
+            return receptionistUser()
+        elif user.tipo_usuario_id.tipo_usuario_id == doctor[0]:
+            return doctorUser()
+        elif user.tipo_usuario_id.tipo_usuario_id == administrator[0]:
+            return administratorUser()
+        elif user.tipo_usuario_id.tipo_usuario_id == patient[0]:
+            return patientUser()
     else:
         return "Nome de usuário ou senha estão incorretos"
 #Função para utilizar no login do front, receber o usuário e mostrar - End
