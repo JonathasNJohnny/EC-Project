e466fa3b4d935024ad2f71d567ed71d9ff3d3137
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-29 17:58:18 -0300

    Update schemes.py

--- NutriHealthBack/schemes/schemes.py
+++ NutriHealthBack/schemes/schemes.py
@@ -9,12 +9,25 @@ class TiposUsuarios(Model):
         database = db
         table_name = 'tipos_usuarios'
 
+class Enderecos(Model):
+    endereco_id = IntegerField(primary_key=True)
+    cidade = CharField(max_length=50, null=False)
+    uf = CharField(max_length=2, null=False)
+    bairro = CharField(max_length=50, null=False)
+    rua = CharField(max_length=50, null=False)
+
+    class Meta:
+        database = db
+
 class Usuarios(Model):
     user_id = IntegerField(primary_key=True)
     user_name = CharField(max_length=50, null=False)
     user_senha = CharField(max_length=50, null=False)
+    user_email = CharField(max_length=50, null=False)
+    user_numero = CharField(max_length=50, null=False)
     cpf = CharField(max_length=50, null=False)
     tipo_usuario_id = ForeignKeyField(TiposUsuarios, field='tipo_usuario_id', backref='usuarios')
+    endereco_id = ForeignKeyField(Enderecos, backref='usuarios', null=True)
 
     class Meta:
         database = db
