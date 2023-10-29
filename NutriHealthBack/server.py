21286dfd5463cfe25c05fb5c4a30e1a08549f2eb
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-29 18:01:24 -0300

    Update server.py

--- NutriHealthBack/server.py
+++ NutriHealthBack/server.py
@@ -1,14 +1,22 @@
 from flask import Flask, request, jsonify
 from functions.login import login
+from functions.returnUsers import getAllUsers
 
 app = Flask(__name__)
 
+#Rota para efetuar o login
 @app.route("/api/login", methods=['GET'])
 def login_route():
     name = request.args.get('name')
     password = request.args.get('password')
     result = login(name, password)
     return jsonify({"message": result})
+
+#Rota para obter todos os usuários
+@app.route("/api/getUsers", methods=['GET'])
+def get_users_route():
+    result = getAllUsers()
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
