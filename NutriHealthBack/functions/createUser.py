1886bbd733fd506dc7906872b2d78a687ba4bb67
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-30 13:29:17 -0300

    Update createUser.py

--- NutriHealthBack/functions/createUser.py
+++ NutriHealthBack/functions/createUser.py
@@ -1,17 +1,19 @@
 from schemes.schemes import*
 
 #Criação de usuário
-def createUser(username, password, cpf, typeUser):
+def createUser(username, email, number, cpf, typeUser, addressCity, addressUF, addressNeighborhood, addressStreet):
     try:
-        # Verificar se o nome de usuário já existe
+        #Verificar se o nome de usuário já existe
         existing_user = Usuarios.get_or_none(Usuarios.user_name == username)
         if existing_user:
             print("O nome de usuário já existe e está sendo usado por outro usuário. Tente novamente com outro nome.")
             return None
 
-        # Criar um novo usuário
-        a=Usuarios.insert(user_name=username, user_senha=password, cpf=cpf, tipo_usuario_id=TiposUsuarios.get(TiposUsuarios.tipo_usuario_nome == typeUser).tipo_usuario_id)
-        a.execute()
+        #Criar Endereco
+        newEndereco=Enderecos.create(cidade = addressCity, uf = addressUF, bairro = addressNeighborhood, rua = addressStreet)
+
+        #Criar um novo usuário
+        newUser = Usuarios.create(user_name=username, user_senha="12345678", user_email=email, user_numero=number, cpf=cpf, tipo_usuario_id=TiposUsuarios.get(TiposUsuarios.tipo_usuario_nome == typeUser).tipo_usuario_id, endereco_id=newEndereco)
 
     except Exception as e:
         print(f"Erro ao criar usuário: {e}")
