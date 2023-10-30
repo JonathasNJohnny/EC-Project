b70286defc4cd7b277627a8e8fdc05b23e45b6f4
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-30 15:23:09 -0300

    Update server.py

--- NutriHealthBack/server.py
+++ NutriHealthBack/server.py
@@ -19,19 +19,22 @@ def get_users_route():
     result = getAllUsers()
     return jsonify(result)
 
-#Rota para cadastrar usuários
-@app.route("/api/registerUser", methods=['GET'])
-def register_user_route():
-    name = request.args.get('name')
-    email = request.args.get('email')
-    number = request.args.get('number')
-    cpf = request.args.get('cpf')
-    typeUser = request.args.get('typeUser')
-    addressCity = request.args.get('addressCity')
-    addressUF = request.args.get('addressUF')
-    addressNeighborhood = request.args.get('addressNeighborhood')
-    addressStreet = request.args.get('addressStreet')
-    return createUser(name, email, number, cpf, typeUser, addressCity, addressUF, addressNeighborhood, addressStreet)
+# Rota para criar um usuário
+@app.route("/api/createUser", methods=['POST'])
+def create_user_route():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        number = data.get('number')
+        cpf = data.get('cpf')
+        typeUser = data.get('typeUser')
+        addressCity = data.get('addressCity')
+        addressUF = data.get('addressUF')
+        addressNeighborhood = data.get('addressNeighborhood')
+        addressStreet = data.get('addressStreet')
+        result = createUser(username, email, number, cpf, typeUser, addressCity, addressUF, addressNeighborhood, addressStreet)
+        return result
 
 if __name__ == "__main__":
     app.run(debug=True)
