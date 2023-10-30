a208e7a7e0934a416303c7f0111cde42e714740c
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-30 13:37:16 -0300

    Update server.py

--- NutriHealthBack/server.py
+++ NutriHealthBack/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from functions.login import login
 from functions.returnUsers import getAllUsers
+from functions.createUser import createUser
 
 app = Flask(__name__)
 
@@ -17,6 +18,20 @@ def login_route():
 def get_users_route():
     result = getAllUsers()
     return jsonify(result)
+
+#Rota para cadastrar usuários
+@app.route("/api/registerUser", methods=['GET'])
+def register_user_route():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    number = request.args.get('number')
+    cpf = request.args.get('cpf')
+    typeUser = request.args.get('typeUser')
+    addressCity = request.args.get('addressCity')
+    addressUF = request.args.get('addressUF')
+    addressNeighborhood = request.args.get('addressNeighborhood')
+    addressStreet = request.args.get('addressStreet')
+    return createUser(name, email, number, cpf, typeUser, addressCity, addressUF, addressNeighborhood, addressStreet)
 
 if __name__ == "__main__":
     app.run(debug=True)
