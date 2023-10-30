9bed9ce56f68c2956b97aca96c78b161a48a6465
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-10-30 16:58:26 -0300

    Update server.py

--- NutriHealthBack/server.py
+++ NutriHealthBack/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from functions.login import login
 from functions.returnUsers import getAllUsers
 from functions.createUser import createUser
+from functions.createAppointment import createAppointment
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ def get_users_route():
     result = getAllUsers()
     return jsonify(result)
 
-# Rota para criar um usuário
+#Rota para criar um usuário
 @app.route("/api/createUser", methods=['POST'])
 def create_user_route():
     if request.method == 'POST':
@@ -35,6 +36,16 @@ def create_user_route():
         addressStreet = data.get('addressStreet')
         result = createUser(username, email, number, cpf, typeUser, addressCity, addressUF, addressNeighborhood, addressStreet)
         return result
+
+#Rota para criar uma consulta
+@app.route("/api/createAppointment", methods=['POST'])
+def create_appointment_route():
+    medico_id = int(request.form.get('medico_id'))
+    paciente_id = int(request.form.get('paciente_id'))
+    data = request.form.get('data')
+    horario = request.form.get('horario')
+    dados = request.form.get('dados')
+    return createAppointment(medico_id, paciente_id, data, horario, dados)
 
 if __name__ == "__main__":
     app.run(debug=True)
