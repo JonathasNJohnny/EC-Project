5b68b87345e7d604ec3de58d049970797332aac1
Kauã <kauamarques52@outlook.com>
2023-11-23 14:04:53 -0300

    atualizaçao dado envio de email automatico e senha

--- NutriHealthBack/functions/automaticEmailGeneration.py
+++ NutriHealthBack/functions/automaticEmailGeneration.py
@@ -1,20 +1,34 @@
+import random
+import string
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+#função para geração de senhas
+def gerar_senha(tamanho=8):
+  caracteres = string.ascii_letters + string.digits + string.punctuation
+  senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+  return senha
+
 
 def enviar_email():
     # Configurações do servidor SMTP do Gmail
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    remetente = "ferreirakaua341@gmail.com"
-    senha = "senhaexemplo"  # Substitua com sua senha ou senha de aplicativo
-    destinatario = "kauamarques52@outlook.com"
+    remetente = "nutrihealth59@gmail.com"
+    senha = "hrxb kmpb avnl fyol"  # Substitua com sua senha ou senha de aplicativo
+    destinatario = "kauamarques52@outlook.com" #substitua  destinatário pelo usuário que esteja fazendo login
 
+
+    # Vamos gerar uma senha de 8 caracteres
+    senha_gerada = gerar_senha(8)
+  
     # Corpo do email em formato HTML
     corpo_email = """
-    <p>olá Kauã</p>
+    <p>olá Kauã,</p>
     <p>Segue o exemplo de teste de emails automáticos </p>
-    """
+    <p>Sua senha é: {}</p>
+    """.format(senha_gerada)
 
     # Criar o objeto MIMEMultipart
     msg = MIMEMultipart()
